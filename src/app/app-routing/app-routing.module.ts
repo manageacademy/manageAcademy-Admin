7d3f52f060991f3ee82b5080fdfd92bd8c6267a3
varikoti <vc.reddy77@gmail.com>
2018-01-19 08:56:24 +0530

    School Registration in dasbord 2 page

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -9,8 +9,8 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      { path: '', redirectTo: 'starter', pathMatch: 'full' },
-      { path: 'starter', component: StarterComponent },
+      { path: '', redirectTo: 'admin', pathMatch: 'full' },
+      { path: 'admin', component: AdminComponent },
     ])
   ],
   declarations: [],
